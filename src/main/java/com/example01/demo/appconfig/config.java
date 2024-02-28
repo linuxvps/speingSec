4c7ea0ecb8ae48c85c670146ae1bed491a35d2b3
alamshenas <linux.vps.vpn@gmail.com>
2024-02-28 11:13:58 +0330

    ine memory add user

--- src/main/java/com/example01/demo/appconfig/config.java
+++ src/main/java/com/example01/demo/appconfig/config.java
@@ -3,6 +3,9 @@ package com.example01.demo.appconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -10,13 +13,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class config {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
 
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("123")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+
+    }
+
 
 }
