fa5792d8f3d150450ca0dd33eb7af2679d800221
alamshenas <linux.vps.vpn@gmail.com>
2024-02-28 18:00:51 +0330

    ine memory add user

--- src/main/java/com/example01/demo/appconfig/config.java
+++ src/main/java/com/example01/demo/appconfig/config.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,8 +26,8 @@ public class config {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+
+        UserDetails admin = User.withUsername("admin")
                 .password("123")
                 .build();
 
@@ -33,5 +35,10 @@ public class config {
 
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder instance = NoOpPasswordEncoder.getInstance();
+        return instance;
+    }
 
 }
